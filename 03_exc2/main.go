c4a3b1ed7d8ceb3000d246e7adb34c6aa2cabba1
bFraley <bFraley@users.noreply.github.com>
2018-08-19 14:29:10 -0400

    03_exc2 - variadic int slice as param, with versions named returns and slice indexs

--- 03_exc2/main.go
+++ 03_exc2/main.go
@@ -2,6 +2,48 @@ package main
 
 import "fmt"
 
-func func main() {
-    
+// Write a function with one variadic parameter that
+// finds the greatest number in a list of numbers.
+
+func main() {
+
+	fmt.Println(largestNum(1, 2, 3, 4, 5))
+	fmt.Println(largestNumNamedReturn(1, 2, 3, 4, 5))
+	fmt.Println(largestNumMultipleNamedReturn(1, 2, 3, 4, 5))
+
+}
+
+func largestNum(nslice ...int) int {
+
+	var largest int
+
+	for _, n := range nslice {
+		if n > largest {
+			largest = n
+		}
+	}
+
+	return largest
+}
+
+func largestNumNamedReturn(nslice ...int) (ans int) {
+	for _, n := range nslice {
+		if n > ans {
+			ans = n
+		}
+	}
+
+	return
+}
+
+func largestNumMultipleNamedReturn(nslice ...int) (loc int, num int) {
+	for i, n := range nslice {
+		if n > num {
+			num = n
+			loc = i
+		}
+	}
+	fmt.Println("Largest num is ", num, "at index ", loc)
+
+	return
 }
