9eb1fd23d161e60262ebb9c9a67bf0481be5adb0
bFraley <bFraley@users.noreply.github.com>
2018-08-19 13:37:20 -0400

    01_exc2 - multiple returns

---
+++ 01_exc2/main.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+    ans1, ans2 := twoReturns(10) // 5, true
+    fmt.Println(ans1, ans2)
+
+    ans3, ans4 := twoNamedReturns(20) // 10, true
+    fmt.Println(ans3, ans4)
+
+    ans5, ans6 := twoReturnsWithFloat(11) // 5.5 false
+    fmt.Println(ans5, ans6)
+}
+
+func twoReturns(n int) (int, bool) {
+    half := n / 2
+    even := n % 2 == 0
+    return half, even
+}
+
+// With named returns 
+func twoNamedReturns(n int) (half int, even bool) {
+    half = n / 2
+    even = n % 2 == 0
+    return
+}
+
+// With Float Division
+func twoReturnsWithFloat(n int) (float64, bool) {
+    return float64(n) / 2, n % 2 == 0
+}
